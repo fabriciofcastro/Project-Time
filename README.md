9f2044a1d61faf02778f353a35b0a41f0ee830f6
Fabricio Castro <fabriciofeitosacastro@gmail.com>
2024-04-13 15:17:34 -0300

    create header

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # Project Timer
-
 ## React + TypeScript + Vite + Styled-Components
 
 01 - Configurar temas 
@@ -7,5 +6,6 @@
 03 - Tipagem de tema
 04 - Configurando Eslint
 05 - React Router Dom
+   1  Navlink to () * É criado de forma automática uma class active e outra aria-current
 06 - Layout de rotas
 
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,15 @@
 import { HeaderContainer, Img } from "./Header.style"
 import logo from '../../assets/logo1.png'
+import { Scroll, Timer } from "phosphor-react"
+import { NavLink } from "react-router-dom"
 
 export const Header = () => {
   return (
     <HeaderContainer>
       <Img src={logo} alt="Logo" />
       <nav>
-        <a href="#">Timer</a>
-        <a href="#">History</a>
+        <NavLink to="/" title="Timer"><Timer size={24}  /></NavLink>
+        <NavLink to="history" title="Histórico"><Scroll size={24} /></NavLink>
       </nav>
     </HeaderContainer>
   )
