dc76272603b869d9097fa64ff678f7b0803801fc
Fabricio Castro <fabriciofeitosacastro@gmail.com>
2024-04-14 13:08:24 -0300

    Create teble history of status

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Project Timer
 ## React + TypeScript + Vite + Styled-Components
 
-01 - Configurar temas 
-02 - Definir temas globais
-03 - Tipagem de tema
+01 - Configurar temas styled-components
+02 - Definir temas globais styled-components
+03 - Tipagem de tema styled-components
 04 - Configurando Eslint
 05 - React Router Dom
    1  Navlink to () * É criado de forma automática uma class active e outra aria-current
--- src/pages/history/style.ts
+++ src/pages/history/style.ts
@@ -61,3 +61,27 @@ export const HistoryList = styled.div`
     }
 
 `
+
+const STATUS_COLORS = {
+  yellow: "yellow-500",
+  green: "green-500",
+  red: "red-500"
+} as const
+
+interface StatusProps {
+  statusColor: keyof typeof STATUS_COLORS
+}
+
+export const Status = styled.span<StatusProps>`
+  display: flex;
+  align-items: center;
+  gap: 0.8rem;
+
+  &::before {
+    content: '';
+    width: .8rem;
+    height: .8rem;
+    border-radius: 999px;
+    background-color: ${(props) => props.theme[STATUS_COLORS[props.statusColor]]}
+  }
+`
